597407692aa3798692fb71fe397908ba9ad6221d
ofrajsingh <rajsingh00seven@gmail.com>
2023-03-07 01:45:46 +0530

    added video tutorials for individual exercises

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -5,7 +5,7 @@ import { borderRadius } from '@mui/system';
 
 const ExerciseCard = ({exercise}) => {
   return (
-    <Link className='exercise-card' to={`/exercise/${exercise.id}`}>
+    <Link className='exercise-card' to={`/exercises/${exercise.id}`}>
         <img src={exercise.gifUrl} alt={exercise.name} loading='lazy'/>
         <Stack direction='row'>
             <Button sx={{ml: '21px', color: '#fff', background: '#ffa9a9', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize'}}>
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,10 +2,19 @@ export const exerciseOptions = {
     method: 'GET',
     headers: {
     //   'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-      'X-RapidAPI-Key': '74ab2ebf38msh57f1e49d90ed853p12a2acjsn8ff0273c600d',
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-    }
-  };
+    'X-RapidAPI-Key': '74ab2ebf38msh57f1e49d90ed853p12a2acjsn8ff0273c600d',
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+  }
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    //   'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': '74ab2ebf38msh57f1e49d90ed853p12a2acjsn8ff0273c600d',
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+  }
+};
 
 
 export const fetchData= async (url, options)=>{
